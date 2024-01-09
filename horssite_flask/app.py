e5aa02f0c61a975974abce86cfabf1cc03c6e646
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-09 15:16:07 +0300

    add static, templates

--- horssite_flask/app.py
+++ horssite_flask/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+menu = [{"name": "Главная", "url": "home"},
+        {"name": "Добавить статью", "url": "add"},
+        {"name": "Обратная связь", "url": "contact"},
+        {"name": "Вход", "url": "entrance"}]
+
+
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html', menu=menu)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
