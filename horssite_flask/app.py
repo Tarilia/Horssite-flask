4b20ce11dfb15dee28d2db67fee84d01a4494001
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-10 15:03:18 +0300

    add contact

--- horssite_flask/app.py
+++ horssite_flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = [{"name": "Главная", "url": "home"},
+menu = [{"name": "Главная", "url": "/"},
         {"name": "Добавить статью", "url": "add"},
         {"name": "Обратная связь", "url": "contact"},
         {"name": "Вход", "url": "entrance"}]
@@ -12,3 +12,13 @@ menu = [{"name": "Главная", "url": "home"},
 def index():
     return render_template('index.html', menu=menu)
 
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form['username'])
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+    return render_template('contact.html', title="Обратная связь", menu=menu)
