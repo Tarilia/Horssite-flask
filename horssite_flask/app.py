afaaa197dc9c79eb3aeb54d45f362b0eb1e4b50d
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-12 15:55:22 +0300

    update app

--- horssite_flask/app.py
+++ horssite_flask/app.py
@@ -2,51 +2,16 @@ from flask import Flask, render_template, url_for, request, flash, session, redi
 from dotenv import load_dotenv
 import os
 
+from horssite_flask.database import get_menu
+
 
 load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-menu = [{"name": "Главная", "url": "/"},
-        {"name": "Добавить статью", "url": "add"},
-        {"name": "Обратная связь", "url": "contact"},
-        {"name": "Вход", "url": "login"}]
-
 
 @app.route('/')
 def index():
+    menu = get_menu()
     return render_template('index.html', menu=menu)
-
-
-@app.route('/contact', methods=["POST", "GET"])
-def contact():
-    if request.method == "POST":
-        print(request.form['username'])
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='error')
-    return render_template('contact.html', title="Обратная связь", menu=menu)
-
-
-@app.errorhandler(404)
-def pageNotFount(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
-
-
-@app.route('/login', methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == 'admin' and request.form['psw'] =="1234":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-    return render_template('login.html', title="Авторизация", menu=menu)
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-    return f"Пользователь: {username}"
