66ea9492a5072ce4e5dc0d87abdc44547db8166d
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-14 16:20:14 +0300

    add register

--- horssite_flask/app.py
+++ horssite_flask/app.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 import os
 import math
 import time
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from horssite_flask.database import get_menu, add_posts, get_post, get_all_posts
+from horssite_flask.database import get_menu, add_posts, get_post, get_all_posts, add_user
 
 
 load_dotenv()
@@ -43,3 +44,31 @@ def show_post(id_post):
     if not title:
         abort(404)
     return render_template('post.html', menu=menu, title=title, post=post)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    menu = get_menu()
+    return render_template("login.html", menu=menu, title="Авторизация")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    menu = get_menu()
+    if request.method == "POST":
+        tm = math.floor(time.time())
+        name = request.form.get('name')
+        email = request.form.get('email')
+        psw = request.form.get('psw')
+        psw2 = request.form.get('psw2')
+        if len(name) > 4 and len(email) > 4 and len(psw) > 4 and psw == psw2:
+            hash = generate_password_hash(psw)
+            result = add_user(name, email, hash, tm)
+            if result:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+             flash("Неверно заполнены поля", "error")
+    return render_template("register.html", menu=menu, title="Регистрация")
