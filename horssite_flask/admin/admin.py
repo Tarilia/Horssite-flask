53b20ae9be635ab2fa5b5ff71ef146a59d540d23
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-19 13:57:01 +0300

    add login admin

--- horssite_flask/admin/admin.py
+++ horssite_flask/admin/admin.py
@@ -1,4 +1,39 @@
-from flask import Blueprint
+from flask import (Blueprint, render_template, url_for,
+                   redirect, session, request, flash, g)
 
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+
+def isLogged():
+    return True if session.get('admin_logged') else False
+
+
+def login_admin():
+    session['admin_logged'] = 1
+
+
+def logout_admin():
+    session.pop('admin_logged', None)
+
+
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
+
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+
+    return render_template('admin/login.html', title='Админ-панель')
